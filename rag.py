6d2c7c1ed977072366b1b69259d9fdd16c86a3c4
qingyang <zhang.qingyang@outlook.com>
2023-03-21 00:04:27 -0700

    final clean

--- rag.py
+++ rag.py
@@ -7,12 +7,15 @@ import json
 from fastrag.stores import PLAIDDocumentStore
 from fastrag.retrievers.colbert import ColBERTRetriever
 from haystack.nodes import SentenceTransformersRanker
+from haystack.nodes import PromptNode
+from haystack.nodes import PromptTemplate
+from haystack.nodes import Shaper
 from fastrag.readers import T5Reader
 from fastrag.readers.FiD import FiDReader
 from fastrag.utils import get_timing_from_pipeline
 from haystack import Pipeline
 
-def jsonify(res, reader=1, k=1, j=3):
+def jsonify(res, reader=1, custom=0, k=1, j=3):
 
     output = {
         'query': res['query'],
@@ -20,7 +23,14 @@ def jsonify(res, reader=1, k=1, j=3):
         'docs': dict()
     }
         
-    if reader:
+    if custom:
+        output['ans']["0"] = {
+            'answer': res['results'],
+            'score': '', 
+            'context': ''
+        }
+
+    if reader & (not custom):
         ans = res['answers'][:k]
         for m, a in enumerate(ans):
             output['ans'][m] = {
@@ -30,6 +40,9 @@ def jsonify(res, reader=1, k=1, j=3):
             }
 
     docs = res['documents'][:j]
+    if custom:
+        docs = res['texts'][:j]
+
     for n, d in enumerate(docs):
         output['docs'][n] = {
             'content': d.content,
@@ -47,34 +60,44 @@ def main():
     parser.add_argument("--ranks", type=int, default=1)
     parser.add_argument("--index", type=int, default=0)
     parser.add_argument("--generative", type=int, default=1)
+    parser.add_argument("--custom", type=int, default=1)
     args = parser.parse_args()
     
     dataroot = 'data'
     dataset = 'effective'
     datasplit = 'train'
 
-    queries_path = os.path.join(dataroot, dataset, datasplit, 'questions.search.tsv')
+    #queries_path = os.path.join(dataroot, dataset, datasplit, 'questions.search.tsv')
+    queries_path = os.path.join(dataroot, dataset, datasplit, 'questions.search.test.tsv')
     collection_path = os.path.join(dataroot, dataset, datasplit, 'collection.tsv')
 
     queries = pd.read_csv(queries_path, sep='\t', header=None)
 
     nbits = 2
-    create = True if args.index else False
     index_name = f'{dataset}.{datasplit}.{nbits}bits'
-    
+    checkpoint_path = "Intel/ColBERT-NQ"
+    if args.index:
+        create = True
+        checkpoint_path = "sebastian-hofstaetter/colbert-distilbert-margin_mse-T2-msmarco"
+        index_name = f'{dataset}.{datasplit}.{nbits}bits.msmarco'
+    else:
+        create = False
+
     #---------------------#
     #  create components  #
     #---------------------#
+
+
     store = PLAIDDocumentStore(
         index_path=index_name,
-        checkpoint_path="Intel/ColBERT-NQ",
+        checkpoint_path=checkpoint_path,
         collection_path=collection_path,
         create=create,
         nbits=nbits,
         gpus=args.gpus,
         ranks=args.ranks,
-        doc_maxlen=120,
-        query_maxlen=60,
+        doc_maxlen=4099,
+        query_maxlen=4099,
         kmeans_niters=4,
     )
     
@@ -87,22 +110,33 @@ def main():
         top_k=10
     )
 
-    # reader = T5Reader(
-    #     model_name_or_path="google/flan-t5-base", 
-    #     input_converter_tokenizer_max_len=16300,  
-    #     min_length=50, 
-    #     max_length=500, 
-    #     num_beams=4, 
-    #     top_k=1, 
-    #     use_gpu=True
-    # )
-
-    reader = FiDReader(
-        input_converter_tokenizer_max_len=250,
-        max_length=20,
-        model_name_or_path="path/to/fid",
+    reader = T5Reader(
+        model_name_or_path="google/flan-t5-base", 
+        input_converter_tokenizer_max_len=16000,  
+        min_length=100, 
+        max_length=512, 
+        num_beams=4, 
+        top_k=1, 
         use_gpu=True
     )
+
+    # custom reader 
+    example_generator = PromptTemplate(
+        name="eg",
+        prompt_text="""Give a better example for this ineffective argument. The example generated should reference similar effective arguments 
+               \n\n Similar effective arguments: $texts \n\nIneffective argument:$query \n\n Answer:"""
+    )
+    prompt_node = PromptNode(model_name_or_path="google/flan-t5-base", default_prompt_template=example_generator, max_length=1000)
+
+    # shaper
+    shaper = Shaper(func="join_documents", inputs={"documents": "documents"}, outputs=["texts"])
+
+    # reader = FiDReader(
+    #     input_converter_tokenizer_max_len=250,
+    #     max_length=20,
+    #     model_name_or_path="path/to/fid",
+    #     use_gpu=True
+    # )
 
     #--------------------#
     #   build pipeline   #
@@ -113,7 +147,12 @@ def main():
     p.add_node(component=retriever, name="Retriever", inputs=["Query"])
     if args.generative:
         p.add_node(component=reranker, name="Reranker", inputs=["Retriever"])
-        p.add_node(component=reader, name="Reader", inputs=["Reranker"])
+        if args.custom:
+            p.add_node(component=shaper, name="Shaper", inputs=["Reranker"])
+            p.add_node(component=prompt_node, name="prompt_node", inputs=["Shaper"])
+        else:
+            p.add_node(component=reader, name="Reader", inputs=["Reranker"])
+
 
     #------------------#
     #   run pipeline   #
@@ -121,15 +160,16 @@ def main():
 
     # store top 3 retrieved docs
     results = dict()
+    tmstp = pd.Timestamp.now()  
     for i in tqdm(range(len(queries))):
         res = p.run(query=queries[1][i])
-        results[i] = jsonify(res, reader=args.generative)
+        print(res)
+        results[i] = jsonify(res, reader=args.generative, custom=args.custom)
     
     # output results into json file
     json_output = json.dumps(results, indent=4) 
-    with open("data/effective/res_t5.json", "w") as outfile:
+    with open(f"data/effective/res_t5_{tmstp}.json", "w") as outfile:
         outfile.write(json_output)
-    
 
 if __name__ == "__main__":
     main()
